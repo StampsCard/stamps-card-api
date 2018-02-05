18c83625e4b1c77c5cabd3819829a362dfcf8e35
dsola <d.sola.03@gmail.com>
2018-02-05 23:22:48 +0100

    Create entity User and add Date scalar types

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,6 +1,12 @@
+const dates = require('graphql-iso-date');
+
 const mutations = require('./mutations');
 const queries = require('./queries');
+
 exports.data = {
     Query: queries.data,
-    Mutation: mutations.data
+    Mutation: mutations.data,
+    DateTime: dates.GraphQLDateTime,
+    Date: dates.GraphQLDate,
+    Time: dates.GraphQLTime
 };
