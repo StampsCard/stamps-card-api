4134a557fa560237d577816ce77acb6571cc9e48
dsola <d.sola.03@gmail.com>
2018-08-11 13:12:17 +0200

    Use prisma 1.13 instead of 1.7 in docker-machine

---
+++ scripts/create_new_docker_machine_for_prisma.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Delete the current machine if exists
+docker-machine rm prisma
+
+# Unset the environment variables
+unset ${!DOCKER*}
+
+# Create a new docker-machine instance
+docker-machine create --driver amazonec2 prisma --amazonec2-region region=eu-central-1
+
+# Define your environment variables
+docker-machine env prisma # Shows the commands
+eval $(docker-machine env prisma) # Execute the commands into your machine
+
+
+# Copy the docker-compose example with 1 instance with the prisma image and another one with mysql
+
+# Create the containers in the AWS instances
+docker-compose up -d
+
+# Display the IP from the machine
+docker-machine ip prisma
+
+# Assign the security groups: RDS, prisma and docker-machine to the AWS instance using the dashboard
