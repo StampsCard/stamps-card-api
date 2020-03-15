c0ad4b893be73fd4b593c13a24177336f7e2eff1
David Sola <d.sola.03@gmail.com>
2020-03-15 23:04:45 +0100

    Prepare project to be deployed with Prisma v1.34

--- scripts/create_new_docker_machine_for_prisma.sh
+++ scripts/create_new_docker_machine_for_prisma.sh
@@ -2,28 +2,26 @@
 
 # Delete the current machine if exists
 docker-machine rm prisma
-
 # Unset the environment variables
 unset ${!DOCKER*}
-
 # Create a new docker-machine instance
-docker-machine create --driver amazonec2 prisma --amazonec2-region region=eu-central-1
-
+docker-machine create --driver amazonec2 --amazonec2-region eu-central-1 prisma
 # Define your environment variables
 docker-machine env prisma # Shows the commands
 eval $(docker-machine env prisma) # Execute the commands into your machine
-
+export COMPOSE_TLS_VERSION=TLSv1_2
 
 # Copy the docker-compose example with 1 instance with the prisma image and another one with mysql
-
 # Create the containers in the AWS instances
 docker-compose up -d
-
 # Display the IP from the machine
 docker-machine ip prisma
-
 # To connect using SSH
 docker-machine ssh prisma
-
-
 # Assign the security groups: RDS, prisma and docker-machine to the AWS instance using the dashboard
+
+# To switch from docker-machine to local
+unset ${!DOCKER*}
+# To local to docker-machine
+eval $(docker-machine env prisma)
+export COMPOSE_TLS_VERSION=TLSv1_2
